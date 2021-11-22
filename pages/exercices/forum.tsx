e1300a1763b5a6141a752d10fb02d1057564ef4e
Lucas Lebailly <lucas.lebailly@telecom-paristech.fr>
2021-11-22 17:04:01 +0100

    début forum

--- pages/exercices/forum.tsx
+++ pages/exercices/forum.tsx
@@ -2,7 +2,37 @@ import type { NextPage } from 'next'
 
 const Exercise4: NextPage = () => {
   return (
-    <p>TODO: Créer le forum ici.</p>
+    <div id="forum-wrapper" className="d-flex flex-column">
+      <div id="message-container"
+      className="flex-grow-1">
+        Messages reçus
+      </div>
+      
+      <div id="new-message-container">
+        Nouveau message
+        <form method="post">
+        <div className="row"> 
+        <div className="col-md">
+      <div className="form-check mb-3">
+        <label className="form-check-label" htmlFor="new-message">Message</label>
+        <input className="form-check" type="text" id="new-message" name="new-message" placeholder="Ecrire votre message..." />
+      </div>
+
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">Envoyer</button> 
+      </div>
+      </div>
+      <div className="col-md">
+      <div className="form-check mb-3">
+        <label className="form-check-label" htmlFor="pseudo">Pseudo</label>
+        <input className="form-check" type="text" id="pseudo" name="pseudo" placeholder="blabla123" />
+      </div>
+      </div>
+      </div>
+      </form>
+      </div>
+      
+    </div>
   )
 }
 
