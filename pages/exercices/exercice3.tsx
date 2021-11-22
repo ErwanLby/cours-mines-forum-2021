6ac10a52bfa3493f8d392635a41a8f7edd43116f
Lucas Lebailly <lucas.lebailly@telecom-paristech.fr>
2021-11-22 16:29:14 +0100

    derniers exos

--- pages/exercices/exercice3.tsx
+++ pages/exercices/exercice3.tsx
@@ -2,15 +2,56 @@ import type { NextPage } from 'next'
 
 const Exercise3: NextPage = () => {
   return (
+    <>
+    <h1> Exercice 3 </h1>
+    
     <p>
-    Exercice 3
-
     Consignes :
-    - Mettre en forme l&apos;exercice 3
-    - Recopier le formulaire d&apos;inscription précédent ci-dessous
-    - Grâce au fichier style.css, mettre le texte des labels en bleu, agrandir la police dans les input texte, placer un
-    bloc en position absolue à droite du champ adresse email avec un texte de votre choix en police 10px rouge.
+    <ul>
+    <li> Mettre en forme l&apos;exercice 3 </li>
+    <li> Recopier le formulaire d&apos;inscription précédent ci-dessous </li>
+    <li> Grâce au fichier style.css, mettre le texte des labels en bleu, agrandir la police dans les input texte, placer un
+    bloc en position absolue à droite du champ adresse email avec un texte de votre choix en police 10px rouge. </li>
+    </ul>
     </p>
+
+    <div>
+      <p id="nouveaubloc">
+        Bon bah là on met un peu de texte
+      </p>
+    </div>
+
+    <form method="post">
+      <div>
+        <label id="firstname-label" htmlFor="firstname">Prénom</label>
+        <input type="text" id="firstname" name="firstname" placeholder="Prénom" />
+      </div>
+
+      <div>
+        <label htmlFor="lastname">Nom</label>
+        <input type="text" id="lastname" name="lastname" placeholder="Nom" />
+      </div>
+
+      <div>
+        <label htmlFor="email">Adresse Mail</label>
+        <input type="email" id="email" name="email" placeholder="tamere@lap*te.fr" />
+      </div>
+
+      <div>
+        <label htmlFor="password">Mot de Passe</label>
+        <input type="password" id="password" name="password" placeholder="..." />
+      </div>
+
+      <div>
+        <label htmlFor="cgu">Vendre son âme au diable</label>
+        <input type="checkbox" id="cgu" name="cgu" />
+      </div>
+
+      <div>
+        <button type="submit">Envoyer</button>
+      </div>
+    </form>
+    </>
   )
 }
 
--- pages/exercices/exercice4.tsx
+++ pages/exercices/exercice4.tsx
@@ -2,15 +2,81 @@ import type { NextPage } from 'next'
 
 const Exercise4: NextPage = () => {
   return (
-    <p>
-    Exercice 4
+    <div>
+      <h1> Exercice 4 </h1>
 
-    Consignes :
-    - Recopier les exercices 1 et 2 ci-dessous
-    - Les adapter pour que les 2 exercices soient sur 2 colonnes différentes sur Desktop, et sur une seule colonne sur
-    Mobile
-    - Adapter le formulaire pour qu&apos;il respecte les guidelines Bootstrap sur les formulaires
-    </p>
+      <p> 
+        Consignes :
+        <ul>
+          <li> Recopier les exercices 1 et 2 ci-dessous </li>
+          <li> Les adapter pour que les 2 exercices soient sur 2 colonnes différentes sur Desktop, et sur une seule colonne sur
+    Mobile </li>
+          <li> Adapter le formulaire pour qu&apos;il respecte les guidelines Bootstrap sur les formulaires </li>
+        </ul>
+      </p>
+    
+    <div className="container">
+      <div className="row">
+        <div className="col-md">
+      <table className="table">
+      <tr>
+        <th>Balise</th>
+        <th>Contenu</th>
+      </tr>
+      <tr>
+        <td> ul </td>
+        <td> de tout (texte,  images, ...) avec des points</td>
+      </tr>
+      <tr>
+        <td> ol </td>
+        <td> de tout, numéroté</td>
+      </tr>
+      <tr>
+        <td> li </td>
+        <td> élément  dans une liste</td>
+      </tr>
+      <tr>
+        <td> p </td>
+        <td> paragraphe de tout</td>
+      </tr>
+      </table>
+      </div>
+      <div className="col-md">
+      <form method="post">
+      <div className="form-check mb-3">
+        <label className="form-check-label" htmlFor="firstname">Prénom</label>
+        <input className="form-check" type="text" id="firstname" name="firstname" placeholder="Prénom" />
+      </div>
+
+      <div className="mb-3 form-check">
+        <label className="form-check-label" htmlFor="lastname">Nom</label>
+        <input className="form-check" type="text" id="lastname" name="lastname" placeholder="Nom" />
+      </div>
+
+      <div  className="form-check mb-3">
+        <label className="form-check-label" htmlFor="email">Adresse Mail</label>
+        <input className="form-check" type="email" id="email" name="email" placeholder="truc@bidule.fr" />
+      </div>
+
+      <div className="form-check mb-3">
+        <label className="form-check-label" htmlFor="password">Mot de Passe</label>
+        <input className="form-check" type="password" id="password" name="password" placeholder="" />
+      </div>
+
+      <div className="form-check mb-3">
+        <input className="form-check-input" type="checkbox" id="cgu" name="cgu" />
+        <label className="form-check-label" htmlFor="cgu">CGU</label> {/* le label après pour la case*/}
+      </div>
+
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">Envoyer</button> {/*class permet d'avoir un joli bouton*/}
+      </div>
+    </form>
+    </div>
+    </div>
+    </div>
+
+    </div>
   )
 }
 
