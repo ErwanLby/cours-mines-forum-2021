110d57784f5b10ad8ade5fdc95f9c9bbd5af62cc
Lucas Lebailly <lucas.lebailly@telecom-paristech.fr>
2021-11-22 13:24:26 +0100

    premiers exos

--- pages/exercices/exercice1.tsx
+++ pages/exercices/exercice1.tsx
@@ -2,15 +2,42 @@ import type { NextPage } from 'next'
 
 const Exercise1: NextPage = () => {
   return (
-    <p>
-      Exercice 1
+    <div>
+    <h1> Exercice 1 </h1>
 
+    <p>
       Consignes :
-      - Mettre en forme l&apos;exercice 1
-      - En-dessous, regrouper les noms de balises vues jusqu&apos;ici dans un tableau avec en-têtes ayant pour colonnes : Balise,
+      <ul>
+      <li> Mettre en forme l&apos;exercice 1 </li>
+      <li> En-dessous, regrouper les noms de balises vues jusqu&apos;ici dans un tableau avec en-têtes ayant pour colonnes : Balise,
       Contenu. Dans la colonne contenu spécifier la ou les balises qu&apos;il est possible d&apos;insérer à l&apos;intérieur, ou &quot;texte&quot;
-      (pour les titres par exemple).
+      (pour les titres par exemple). </li>
+      </ul>
     </p>
+
+    <table className="table">
+      <tr>
+        <th>Balise</th>
+        <th>Contenu</th>
+      </tr>
+      <tr>
+        <td> ul </td>
+        <td> de tout (texte,  images, ...) avec des points</td>
+      </tr>
+      <tr>
+        <td> ol </td>
+        <td> de tout, numéroté</td>
+      </tr>
+      <tr>
+        <td> li </td>
+        <td> élément  dans une liste</td>
+      </tr>
+      <tr>
+        <td> p </td>
+        <td> paragraphe de tout</td>
+      </tr>
+      </table>
+    </div>
   )
 }
 
--- pages/exercices/exercice2.tsx
+++ pages/exercices/exercice2.tsx
@@ -2,14 +2,49 @@ import type { NextPage } from 'next'
 
 const Exercise2: NextPage = () => {
   return (
+    <>
+    
+    <h1> Exercice 2 </h1>
     <p>
-    Exercice 2
-
     Consignes :
-    - Mettre en forme l&apos;exercice 2
-    - Créer ci-dessous un formulaire d&apos;inscription contenant les champs : Prénom, nom, email, mot de passe, checkbox pour
-    accepter un quelconque règlement et un bouton de validation.
+    <ul>
+    <li> Mettre en forme l&apos;exercice 2 </li>
+    <li> Créer ci-dessous un formulaire d&apos;inscription contenant les champs : Prénom, nom, email, mot de passe, checkbox pour
+    accepter un quelconque règlement et un bouton de validation. </li>
+    </ul>
     </p>
+
+    <form method="post">
+      <div>
+        <label htmlFor="firstname">Prénom</label>
+        <input type="text" id="firstname" name="firstname" placeholder="Prénom" />
+      </div>
+
+      <div>
+        <label htmlFor="lastname">Nom</label>
+        <input type="text" id="lastname" name="lastname" placeholder="Nom" />
+      </div>
+
+      <div>
+        <label htmlFor="email">Adresse Mail</label>
+        <input type="email" id="email" name="email" placeholder="tamere@lap*te.fr" />
+      </div>
+
+      <div>
+        <label htmlFor="password">Mot de Passe</label>
+        <input type="password" id="password" name="password" placeholder="" />
+      </div>
+
+      <div>
+        <label htmlFor="cgu">Vendre son âme au diable</label>
+        <input type="checkbox" id="cgu" name="cgu" />
+      </div>
+
+      <div>
+        <button type="submit">Envoyer</button>
+      </div>
+    </form>
+    </>
   )
 }
 
